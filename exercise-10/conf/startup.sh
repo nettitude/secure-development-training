5a8274f447e3075a421e7869342eccb9e3c6e70f
thackeraaron <thackeraaron@users.noreply.github.com>
2023-01-10 12:06:29 +0000

    Updated challenges

--- exercise-10/conf/startup.sh
+++ exercise-10/conf/startup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-php composer.phar require mikehaertl/phpwkhtmltopdf
-service apache2 restart
+service apache2 restart > /dev/null
 chmod 777 pdfs
-bash -i
+
+echo "  ______                   _            __  ___  "
+echo " |  ____|                 (_)          /_ |/ _ \ "
+echo " | |__  __  _____ _ __ ___ _ ___  ___   | | | | |"
+echo " |  __| \ \/ / _ \ '__/ __| / __|/ _ \  | | | | |"
+echo " | |____ >  <  __/ | | (__| \__ \  __/  | | |_| |"
+echo " |______/_/\_\___|_|  \___|_|___/\___|  |_|\___/ "
+echo "                                                 "
+echo "Press Ctrl+C to exit."
+echo ""
+
 tail -F /var/log/apache2/access.log
--- exercise-6/conf/startup.sh
+++ exercise-6/conf/startup.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 service apache2 restart
 service mysql restart
-bash -i
-
+echo "  ______                   _              __  "
+echo " |  ____|                 (_)            / /  "
+echo " | |__  __  _____ _ __ ___ _ ___  ___   / /_  "
+echo " |  __| \ \/ / _ \ '__/ __| / __|/ _ \ | '_ \ "
+echo " | |____ >  <  __/ | | (__| \__ \  __/ | (_) |"
+echo " |______/_/\_\___|_|  \___|_|___/\___|  \___/ "
+echo ""
+echo "Press Ctrl+C to exit."
+echo ""
+( trap exit SIGINT ; read -r -d '' _ </dev/tty )
